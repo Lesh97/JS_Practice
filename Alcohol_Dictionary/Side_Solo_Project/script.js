ffb76d7b4646ddaab065308ce3c8a998e1ab30b8
Lesh97 <dltngus4652@gmail.com>
2022-12-30 11:31:12 +0900

    popup layer

--- Alcohol_Dictionary/Side_Solo_Project/script.js
+++ Alcohol_Dictionary/Side_Solo_Project/script.js
@@ -15,42 +15,17 @@ sidebarBtn.addEventListener("click", () => {
 
 /** dimmed pop-up layer for porduct infomation */
 
-$(".btn-example1").click(function () {
-  var $href = $(this).attr("href");
-  layer_popup($href);
-});
-function layer_popup(el) {
-  console.log(el);
-  var $el = $(el); //레이어의 id를 $el 변수에 저장
-  var isDim = $el.prev().hasClass("dimBg"); //dimmed 레이어를 감지하기 위한 boolean 변수
-
-  isDim ? $(".dim-layer1").fadeIn() : $el.fadeIn();
-
-  var $elWidth = ~~$el.outerWidth(),
-    $elHeight = ~~$el.outerHeight(),
-    docWidth = $(document).width(),
-    docHeight = $(document).height();
-
-  // 화면의 중앙에 레이어를 띄운다.
-  if ($elHeight < docHeight || $elWidth < docWidth) {
-    $el.css({
-      marginTop: -$elHeight / 2,
-      marginLeft: -$elWidth / 2,
-    });
-  } else {
-    $el.css({ top: 0, left: 0 });
-  }
-
-  $el.find("a.btn-layerClose").click(function () {
-    isDim ? $(".dim-layer1").fadeOut() : $el.fadeOut(); // 닫기 버튼을 클릭하면 레이어가 닫힌다.
-    return false;
-  });
-
-  $(".layer .dimBg").click(function () {
-    $(".dim-layer1").fadeOut();
-    return false;
-  });
+//팝업 열기
+function openModal(modalname) {
+  console.log(modalname);
+  document.get;
+  $("." + modalname).fadeIn(300);
 }
+
+// 팝업 닫기
+$(".popup .close").click(function () {
+  $(this).parent().fadeOut(300);
+});
 
 /*$(".btn-example").click(function () {
   let $href = $(this).attr("href");
